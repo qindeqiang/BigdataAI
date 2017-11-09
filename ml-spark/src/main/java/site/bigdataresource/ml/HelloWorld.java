7294778c9f49d63e1a6496994a48005fb93ea7c3
qindeqiang <qindeqiang1@163.com>
2017-11-09 04:50:47 -0500

    Init Project Local

---
+++ ml-spark/src/main/java/site/bigdataresource/ml/HelloWorld.java
@@ -0,0 +1,11 @@
+package site.bigdataresource.ml;
+
+/**
+ * Created by deqiangqin@gmail.com on 11/9/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld,ML");
+    }
+}
